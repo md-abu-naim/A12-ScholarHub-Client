137fcdef2905376a2500b305f70d8a7b763f8c8d
abunaim34 <farhanadnanfarabi@gmail.com>
2024-06-01 16:43:35 +0600

    view booked session details page design

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -2,39 +2,41 @@ import { Link } from 'react-router-dom';
 import Navber from '../Shared/Navber';
 
 const SignUp = () => {
+
+    const handleSignUp = e => {
+        e.preventDefault()
+        const form = e.target
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value;
+        const role = form.role.value
+        const user = {name, email, password, role}
+        console.log(user);
+
+    }
     return (
         <>
             <Navber />
             <div className="bg-black py-8 ">
-                {/* <Helmet>
-                <title>GRANNY | SIGN UP</title>
-            </Helmet> */}
-
                 <div className="w-full  font-serif max-w-md p-6 text-white m-auto mx-auto bg-[#1B1616] rounded-2xl shadow-md dark:bg-gray-800">
                     <h2 className='text-2xl my-3 font-bold text-center'>ScholarHub</h2>
                     <h2 className='text-2xl my-3 font-bold text-center'>Please SignUp.</h2>
-
-                    <form >
+                    <form onSubmit={handleSignUp}>
                         <div className="pb-2">
                             <label className="block text-sm text-white dark:text-gray-200">Name</label>
                             <input type="text" name='name' placeholder='Enter Full Name' className="block w-full px-4 py-2 text-white bg-gray-600 border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
-                        <div className="pb-2">
+                        <div>
                             <label className="block text-sm text-white dark:text-gray-200">Email</label>
                             <input type="email" name='email' placeholder='Enter your Email' className="block w-full px-4 py-2 text-white bg-gray-600 border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
-                        <div className="pb-2">
-                            <label className="block text-sm text-white dark:text-gray-200">Photo URL</label>
-                            <input type="text" name='photo' placeholder='Enter your PhotoURL' className="block w-full px-4 py-2 text-white bg-gray-600 border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-                        </div>
-
                         <div className="mt-4">
                             <label className="block text-sm text-white dark:text-gray-200">Password</label>
                             <input type="password" name='password' placeholder='Enter your Password' className="block w-full px-4 py-2 mt-2 text-white bg-gray-600 border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
                         <div className='mt-4'>
                             <label className="block text-sm text-white dark:text-gray-200">Role</label>
-                            <select className="select select-ghost w-full bg-gray-600 border rounded-lg text-white ">
+                            <select name='role'  className="select select-ghost w-full bg-gray-600 border rounded-lg text-white ">
                                 <option disabled selected>Select your Role</option>
                                 <option className='text-white'>Admin</option>
                                 <option className='text-white'>Tutor</option>
