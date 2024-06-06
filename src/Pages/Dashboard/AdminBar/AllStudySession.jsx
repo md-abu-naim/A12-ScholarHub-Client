6fa2606bbfa0f95c487297aa9aa49e17fdb3e457
abunaim34 <farhanadnanfarabi@gmail.com>
2024-06-06 17:00:43 +0600

    this is session approval work

--- src/Pages/Dashboard/AdminBar/AllStudySession.jsx
+++ src/Pages/Dashboard/AdminBar/AllStudySession.jsx
@@ -1,9 +1,97 @@
 import SectionTitle from "../../../Shared/SectionTitle";
+import CommonBtn from "../../../Shared/CommonBtn";
+import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FcApprove, FcDisapprove } from "react-icons/fc";
+import { useState } from "react";
+import Approve from "./Modals/Approve";
+import toast from "react-hot-toast";
+// import Approve from "./Modals/Approve";
 
 const AllStudySession = () => {
+    const [approveId, setApproveId] = useState('')
+    const axiosSecure = useAxiosSecure()
+
+    const handleModal = (id) => {
+        setApproveId(id)
+        document.getElementById('my_modal_3').showModal()
+    }
+
+    const { data: allSessions = [], refetch } = useQuery({
+        queryKey: ['allSessions'],
+        queryFn: async () => {
+            const { data } = await axiosSecure.get(`/allSessions`)
+            return data
+        }
+    })
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        const registration_fee = e.target.registration_fee.value
+        const status = 'Approve'
+        const id = approveId
+        const updateInfo = {
+            registration_fee: registration_fee,
+            status: status
+        }
+        e.target.reset()
+        console.log(id, updateInfo);
+        axiosSecure.put(`/session/${id}`, updateInfo)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount > 0) {
+                    refetch()
+                    toast.success('Session has been Approve!🤩')
+                }
+            })
+    }
     return (
         <div>
             <SectionTitle heading='view all study session' subHeading='This is our all study session page' />
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+                {
+                    allSessions.map((session, i) => <div key={i} className='w-full rel max-h-[300px] max-w-md  px-4 py-3 bg-[#1B1616] rounded-md shadow-md hover:scale-[1.05] transition-all flex flex-col'>
+                        <div className="flex-grow">
+                            <div className='flex items-center justify-between'>
+                                <span className='text-xs font-light text-white '>
+                                    Registration end: {session.registration_end_date}
+                                </span>
+                                <span className='text-xs font-light bg-[#C39C5D] rounded-full px-2 text-black '>
+                                    {session.status}...
+                                </span>
+                            </div>
+
+                            <div>
+                                <h1 className='mt-2 text-lg font-semibold text-white '>
+                                    {session.session_title}
+                                </h1>
+
+                                <p className='mt-2 text-sm text-white '>
+                                    {session.description}
+                                </p>
+
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-between mt-4">
+                            <button onClick={() => handleModal(session._id)} title="Approve Session"><CommonBtn title={<FcApprove className="text-2xl " />} /></button>
+                            <button title="Reject Session"><CommonBtn title={<FcDisapprove className="text-xl" />} /></button>
+                        </div>
+                        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                        {/* <button className="btn">open modal</button> */}
+                        {/* <dialog id="my_modal_3" className="modal">
+                            <div className="modal-box">
+                                <form method="dialog">
+                                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                </form>
+                                <h3 className="font-bold text-lg">Hello!</h3>
+                                <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                                <button onClick={ handleClick}>onClick</button>
+                            </div>
+                        </dialog> */}
+                        <Approve handleClick={handleClick} title={session.session_title} />
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Dashboard/AdminBar/Modals/Approve.jsx
@@ -0,0 +1,25 @@
+
+const Approve = ({ handleClick, title }) => {
+    return (
+        <dialog id="my_modal_3" className="modal ">
+            <div className="modal-box bg-[#1B1616] text-white">
+                <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
+                <div className="text-center">
+                    <h3 className="font-bold text-xl">{title}</h3>
+                    <form onSubmit={handleClick} className="mt-6 text-start space-y-2" >
+                    <span className="md:pl-11">Registration Free or Paid..?</span>
+                        <label className="input bg-[#222222] input-bordered flex items-center md:max-w-sm md:mx-auto  gap-2">
+                            <input type="text" name="registration_fee" className="grow text-white" placeholder="Registration Fee" />
+                            <button className="btn bg-[#c59d5f] hover:bg-[#f7ce8c]  border-none text-black font-bold">Submit</button>
+                        </label>
+                    </form>
+                    {/* <button onClick={handleClick}>onClick</button> */}
+                </div>
+            </div>
+        </dialog>
+    );
+};
+
+export default Approve;
